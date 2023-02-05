518ce2507da237decc77bc48b457a2a19581c39f
summerorange <hyang9387@gmail.com>
2023-02-05 15:17:47 +0900

    SQLite3 CREATE and UPDATE testing

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import Flask, render_template, request, url_for, flash, redirect, abort
 from secret_key import SECRET_KEY
 
 app = Flask(__name__)
@@ -10,6 +10,15 @@ def get_db_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
+def get_post(post_id):
+    connection = get_db_connection()
+    post = connection.execute('SELECT * FROM posts WHERE id=?',
+                              (post_id, )).fetchone()
+    connection.close()
+    if post is None:
+        abort(404)
+    return post
+
 @app.route('/')
 def index():
     connection = get_db_connection()
@@ -19,5 +28,45 @@ def index():
 
 # export FLASK_APP=app
 # export FLASK_ENV=development
+
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required')
+        elif not content:
+            flash('Content is required')
+        else:
+            connection = get_db_connection()
+            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                               (title, content))
+            connection.commit()
+            connection.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+@app.route('/<int:id>/edit/', methods=['GET', 'POST'])
+def edit(id):
+    post = get_post(id)
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title=?, content=?'
+                         ' WHERE id=?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('edit.html', post=post)
 
 app.run(debug=True)
